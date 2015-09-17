f21dfb3e23dbb988a2a045b63fa60a4d7b722597
vanshkumar <vanshkumar95@gmail.com>
2015-09-17 15:26:06 -0700

    added slideshow editing form

--- public-src/app/controllers/edit-slideshow.js
+++ public-src/app/controllers/edit-slideshow.js
@@ -1,4 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+
+    actions : {
+        addLink: function(link_url) {
+            var new_link = this.store.createRecord('link',
+                {url: link_url,
+                title: '',
+                edit_code: this.get('model.edit_code')});
+
+            new_link.save();
+        },
+
+        shareSlideshow: function() {
+            
+        }
+    }
+
 });
---
+++ public-src/app/controllers/index.js
@@ -0,0 +1,14 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+    
+    actions : {
+        makeSlideshow: function() {
+          var slideshow = this.store.createRecord('slideshow', {title: 'test'});
+
+          slideshow.save().then(function(slideshowModel) {
+            this.transitionTo('edit-slideshow', slideshowModel.get('edit_code'));
+          });
+        }        
+    }
+});
